50cfa788fdb58b872ff51be6988b673706abf471
Kirill777-web <ki.lazutin@gmail.com>
2024-01-12 06:21:03 -0400

    Added resolvers and typeDefs for ProductStat

--- server/models/ProductStat.js
+++ server/models/ProductStat.js
@@ -1,42 +1,44 @@
-import { Schema, model } from "mongoose";
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const model = mongoose.model;
 
 const ProductStatSchema = new Schema(
   {
     productId: {
       type: Schema.Types.ObjectId,
-      ref: "Product"
+      ref: 'Product',
     },
 
     yearlySalesTotal: {
       type: Number,
-      required: true
+      required: true,
     },
     yearlyTotalSoldUnits: {
       type: Number,
-      required: true
+      required: true,
     },
     year: {
       type: Number,
-      required: true
+      required: true,
     },
     monthlyData: [
       {
         month: String,
         totalSales: Number,
-        totalUnits: Number
-      }
+        totalUnits: Number,
+      },
     ],
     dailyData: [
       {
         date: String,
         totalSales: Number,
-        totalUnits: Number
-      }
-    ]
+        totalUnits: Number,
+      },
+    ],
   },
   { timestamps: true }
 );
 
-const ProductStat = model("ProductStat", ProductStatSchema);
+const ProductStat = model('ProductStat', ProductStatSchema);
 
 module.exports = ProductStat;
