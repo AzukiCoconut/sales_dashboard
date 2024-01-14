0692c6c82aa2a96131a38ec18c194480853f058c
Don-Patey <139824991+Don-Patey@users.noreply.github.com>
2024-01-14 13:37:46 -0400

    apollo and graphql into app.jsx and imported usegetsalesquery to breakdown chart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,50 @@
+import './App.css';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
+import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+// link to GraphQL server
+
+// middleware to retrieve token from localStorage and set the request headers before making the GraphQL API request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// instantiate ApolloClient
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+// wrap the entire application in the ApolloProvider component to make every request work with the ApolloClient instance we created 
 function App() {
   return (
-    <>
-      <h1>React App</h1>
-    </>
+    <ApolloProvider client={client}>
+      <Navbar />
+      <Outlet />
+    </ApolloProvider>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { useRoutes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import routes from 'routes';
+import Navbar from './components/Navbar';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//TODO Define your GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Set up Apollo Client with authentication token
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const theme = createTheme();
+
+function App() {
+  const routing = useRoutes(routes);
+
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        {routing}
+      </ThemeProvider>
+    </ApolloProvider>
+  );
+}
+
+export default App;
