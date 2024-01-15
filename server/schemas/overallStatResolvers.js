47f596e043507e64e81e5d1cafb68c90483cc6db
AzukiCoconut <136940320+AzukiCoconut@users.noreply.github.com>
2024-01-15 19:26:57 -0400

    Added dashboard to the project with connection to database

--- server/schemas/overallStatResolvers.js
+++ server/schemas/overallStatResolvers.js
@@ -1,11 +1,47 @@
-const { OverallStat } = require('../models');
+const { OverallStat, Transaction } = require("../models");
 
 const overallStatResolvers = {
   //all Stats and single Stat with id
   Query: {
     overallStats: async () => await OverallStat.find({}),
     overallStat: async (_, { _id }) => await OverallStat.findById(_id),
-  },
+    getDashboardStats: async () => {
+      const currentMonth = "November";
+      const currentYear = "2021";
+      const currentDay = "2021-11-15";
+
+      const transactions = await Transaction.find();
+
+      const overallStat = await OverallStat.find({ year: currentYear });
+
+      const {
+        totalCustomers,
+        yearlyTotalSoldUnits,
+        yearlySalesTotal,
+        monthlyData
+      } = overallStat[0];
+
+      const thisMonthStats = overallStat[0].monthlyData.find(({ month }) => {
+        return month === currentMonth;
+      });
+
+      const todayStats = overallStat[0].dailyData.find(({ date }) => {
+        return date === currentDay;
+      });
+
+      const dashboardStats = {
+        totalCustomers: totalCustomers,
+        yearlyTotalSoldUnits: yearlyTotalSoldUnits,
+        yearlySalesTotal: yearlySalesTotal,
+        monthlyData: monthlyData,
+        thisMonthStats: thisMonthStats,
+        todayStats: todayStats,
+        transactions
+      };
+
+      return dashboardStats;
+    }
+  }
 };
 
 module.exports = overallStatResolvers;
