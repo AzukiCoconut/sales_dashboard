cc355b6790da7c1817f4f4787b0563b79836be1f
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2024-01-16 18:02:26 -0400

    update signup variable -firstname lastname- to name, in order to match the User defination of typeDefs, also changed the style of page signup form

--- client/src/scenes/signup/index.jsx
+++ client/src/scenes/signup/index.jsx
@@ -61,25 +61,15 @@ export default function Signup() {
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12}>
                 <TextField
                   autoComplete="given-name"
-                  name="firstName"
+                  name="username"
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
+                  id="userName"
+                  label="User Name"
                   autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
                 />
               </Grid>
               <Grid item xs={12}>
